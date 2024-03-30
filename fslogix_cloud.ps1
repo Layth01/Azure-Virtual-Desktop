d74a03c9946cf239e10a5a6a75e23bb0ee88cc63
Layth CHEBBI <layth-chebbi@outlook.com>
2024-03-31 00:02:11 +0100

    updated fslogix script file

--- fslogix_cloud.ps1
+++ fslogix_cloud.ps1
@@ -1,14 +1,52 @@
-write-host "Configuring FSLogix"
-$fileServer="xxx.file.core.windows.net"
-$profileShare="\\$($fileServer)\profiles"
+# Display a message indicating the start of FSLogix configurations
+write-host "Starting FSLogix configurations"
 
-$user="localhost\xxx"
-$secret="The Storage Account Access Key"
+# Define the file share location on Azure
+$fileShare=""
+# Construct the profile file share path using the defined file share location
+$profilefileShare="\\$($fileShare)\profiles"
+# Define the user for the Azure Virtual Desktop
+$user=""
+# Define the access key for the Azure Virtual Desktop
+$accesskey=""
+
+# Create a new registry key for FSLogix under SOFTWARE, ignore if it already exists
+New-Item -Path "HKLM:\SOFTWARE" -Name "FSLogix" -ErrorAction Ignore
+# Create a new registry key for Profiles under FSLogix, ignore if it already exists
+New-Item -Path "HKLM:\SOFTWARE\FSLogix" -Name "Profiles" -ErrorAction Ignore
+# Enable FSLogix Profiles
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value 1 -force
+# Set the location for VHD files
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $profilefileShare -force
+# Allow concurrent user sessions
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ConcurrentUserSessions" -Value 1 -force
+# Delete the local profile when a VHD should be applied
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -force
+# Change the profile directory name on each login/logout
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value 1 -force
+# Enable dynamic VHD(X) disk type
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "IsDynamic" -Value 1 -force
+# Do not keep a local copy of the profile directory
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "KeepLocalDir" -Value 0 -force
+# Set the profile type to local
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ProfileType" -Value 0 -force
+# Set the maximum size of the VHD(X) in MBs
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "SizeInMBs" -Value 40000 -force
+# Set the disk type to VHDX
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Value "VHDX" -force
+# Access the network as a computer object
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "AccessNetworkAsComputerObject" -Value 1 -force
+
+# Add the storage account credentials to the Windows Credential Manager
+cmdkey.exe /add:$fileShare /user:$($user) /pass:$($accesskey)
+# Turn off Windows Defender Credential Guard, this step is only necessary for Windows 11 22H2
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -force
+write-host "FSLogix has been configured successfully"
 
 New-Item -Path "HKLM:\SOFTWARE" -Name "FSLogix" -ErrorAction Ignore
 New-Item -Path "HKLM:\SOFTWARE\FSLogix" -Name "Profiles" -ErrorAction Ignore
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value 1 -force
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $profileShare -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $profilefileShare -force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ConcurrentUserSessions" -Value 1 -force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value 1 -force
@@ -20,8 +58,13 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Val
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "AccessNetworkAsComputerObject" -Value 1 -force
 
 # Store credentials to access the storage account
-cmdkey.exe /add:$fileServer /user:$($user) /pass:$($secret)
+cmdkey.exe /add:$fileShare /user:$($user) /pass:$($accesskey)
 # Disable Windows Defender Credential Guard (only needed for Windows 11 22H2)
 New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -force
 
-write-host "The script has finished."
+# Add the storage account credentials to the Windows Credential Manager
+cmdkey.exe /add:$fileShare /user:$($user) /pass:$($accesskey)
+# Turn off Windows Defender Credential Guard, this step is only necessary for Windows 11 22H2
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -force
+
+write-host "FSLogix has been configured successfully"
